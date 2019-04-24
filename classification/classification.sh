77d8061a16a95108287c879b77578627da186b76
aleluc91 <aleluc.bsn@gmail.com>
2019-04-24 11:42:49 +0200

    Update classification script

--- classification/classification.sh
+++ classification/classification.sh
@@ -7,7 +7,7 @@ documents="no"
 model="$SCRIPTDIR/Senti4SD_model.clf"
 chunkSize=200
 jobsNumber=1
-outputFile="$SCRIPTDIR/prediction.csv"
+outputFile="$SCRIPTDIR/predictions.csv"
 
 help(){
     echo "Usage: classificationTask.sh -i input.csv [-d documents] [-m model] [-c chunk_size] [-j jobs_number] [-o predictions.csv]"
@@ -16,7 +16,7 @@ help(){
     echo "-m trained model [optional] [default = Senti4SD_model]"
     echo "-c chunk size [optional] [defaul = 500]"
     echo "-j number of jobs for parallelism [optional] [default = 1]"
-    echo "-o output file with predicted label [optional] [default = prediction.csv]"
+    echo "-o output file with predicted label [optional] [default = predictions.csv]"
     exit 1
 }
 
@@ -65,6 +65,12 @@ if [ ! -f $inputFile ]; then
     exit 1
 else 
 
+    python $SCRIPTDIR/csv_processing.py -i $inputFile
+
+    IFS='.' read -ra FILENAMESPLIT <<< "$inputFile"
+    jarInputFile="${FILENAMESPLIT[0]}_jar.csv"
+    echo $jarInputFile
+
     #-F A: all features to be considered
     #-i file_name: a file containg a document for every line
     #-W cbow600.bin: DSM to be loaded
@@ -74,16 +80,12 @@ else
     #-ul file_name: unigram's list to use for feature extraction. If not present default Senti4SD unigram's list will be used [optional]
     #-bl file_name: bigram's list to use for feature extraction. If not present default Senti4SD bigram's list will be used [optional]
 
-    #java -jar $SCRIPTDIR/Senti4SD.jar -F A -i $1 -W $SCRIPTDIR/dsm.bin -oc $SCRIPTDIR/extractedFeatures.csv -vd 600
-    java -jar $SCRIPTDIR/Senti4SD-fast.jar -F A -i $inputFile -W $SCRIPTDIR/dsm.bin -oc $SCRIPTDIR/extractedFeatures.csv -vd 600
+    java -jar $SCRIPTDIR/Senti4SD-fast.jar -F A -i $jarInputFile -W $SCRIPTDIR/dsm.bin -oc $SCRIPTDIR/extractedFeatures.csv -vd 600
 
-    if [ $documents == "yes" ]; then
-      python $SCRIPTDIR/classification.py -i $SCRIPTDIR/extractedFeatures.csv -i $inputFile -m $model -c $chunkSize -j $jobsNumber -o $outputFile
-    else
-      python $SCRIPTDIR/classification.py -i $SCRIPTDIR/extractedFeatures.csv -m $model -c $chunkSize -j $jobsNumber -o $outputFile
-    fi
-
+    python $SCRIPTDIR/classification.py -i $SCRIPTDIR/extractedFeatures.csv -i $inputFile -d $documents -m $model -c $chunkSize -j $jobsNumber -o $outputFile
+    
     rm $SCRIPTDIR/extractedFeatures.csv
+    rm $jarInputFile
 
 fi
 fi
